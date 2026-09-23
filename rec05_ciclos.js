6639d4bc60341d7bd6a3b6b2848730204ca307d9
Gustavosathler421 <gustavosathler42@gmail.com>
2026-09-23 14:54:11 -0300

    ex.5

---
+++ rec05_ciclos.js
@@ -0,0 +1,13 @@
+
+const entrada = require('readline-sync');
+
+const produtosPorCiclo = entrada.questionInt("Digite quantos produtos sao produzidos por ciclo: ");
+
+ console.log("\n=== PRODUÇÃO ACUMULADA ===");
+ 
+for (let ciclo = 1; ciclo <= 12; ciclo++) {
+    const producaoAcumulada = produtosPorCiclo * ciclo;
+
+    console.log(`Ciclo ${ciclo}: ${producaoAcumulada} produtos acumulados`);
+}
+
