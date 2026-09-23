9f3da43017574fdfd013ef78a4502b8c79a2e5c2
Gustavosathler421 <gustavosathler42@gmail.com>
2026-09-23 13:49:53 -0300

    ex.1

---
+++ rec01_producao.js
@@ -0,0 +1,10 @@
+const caixasPorHora = 75;
+const horasTrabalhadas = 8;
+
+const totalProduzido = caixasPorHora * horasTrabalhadas;
+
+console.log(`Caixas por hora: ${caixasPorHora}`);
+console.log(`Horas trabalhadas: ${horasTrabalhadas}`);
+console.log(`Total produzido: ${totalProduzido} caixas`);
+
+
